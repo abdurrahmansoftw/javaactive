1f06de804171ab3755a2ba8c0df6cbf9a7a6bee3
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-18 01:10:09 +0600

    create spring backend

---
+++ amigos/src/test/java/abdurrahman/amigos/AmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package abdurrahman.amigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
