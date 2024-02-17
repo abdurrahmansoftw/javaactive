0ce8065012b46b1572d1c7bf5414569fe2cdbd4e
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-18 01:34:46 +0600

    add amigoscode Hello restfull

--- amigos/src/main/java/abdurrahman/amigos/AmigosApplication.java
+++ amigos/src/main/java/abdurrahman/amigos/AmigosApplication.java
@@ -2,11 +2,18 @@ package abdurrahman.amigos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AmigosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmigosApplication.class, args);
+    }
+    @GetMapping("/")
+    public String greet() {
+        return "Hello restfull api is running ";
     }
 }
